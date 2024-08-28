78d42d61b4b8db8e840fa953f3aedfa40a096f02
WillePille69xx <william.grenholm@elev.ga.ntig.se>
2024-08-28 09:29:48 +0200

    Variabler

---
+++ lektion-1.py
@@ -0,0 +1,6 @@
+namn = input("Skriv ditt namn: ")
+info = "Tecknet vi använder till strängar heter fnuttar"
+print(f"{info} och det ska {namn} komma ihåg!")
+print("Hello", namn)
+
+print(type(namn))
